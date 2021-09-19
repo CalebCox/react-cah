e5d098fdbdc87eef433ad24d675115f55f209101
Caleb Hypes <hello@calebhypes.com>
2021-09-19 03:28:08 -0400

    Added folder structure, setup NextJS

---
+++ client/pages/_app.js
@@ -0,0 +1,9 @@
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import IndexPage from '.';
+
+export default function MyApp({ Component, pageProps }) {
+    return (
+        <IndexPage />
+    );
+}
